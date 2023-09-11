8960a5d0aad0b695f7336346f64d30740bf9734c
matushitaB <matushitaB@robocon.gifu2@gmail.com>
2023-09-11 20:45:44 +0900

    ok

--- ArduinoMega-MotorAirSwitchSlave-Haichusetsu-CatchRobo/src/main_MotorAirSwitch.ino
+++ ArduinoMega-MotorAirSwitchSlave-Haichusetsu-CatchRobo/src/main_MotorAirSwitch.ino
@@ -5,10 +5,12 @@ void setup() {
   Serial1.begin(115200);
   air_setup();
   motor_setup();
+  switch_setup();
 }
 
 void loop(){
   receive(&Serial1);
+  swtich_input();
   debug_dataprint(&Serial);
   air_output();
   motor_output();
