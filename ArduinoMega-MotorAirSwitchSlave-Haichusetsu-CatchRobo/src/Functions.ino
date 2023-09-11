6181b622fa04abdc3a68abf7dfe45addb3f99c37
masayasuda <masayasuda137@gmail.com>
2023-09-11 19:32:21 +0900

    ok

--- ArduinoMega-MotorAirSwitchSlave-Haichusetsu-CatchRobo/src/Functions.ino
+++ ArduinoMega-MotorAirSwitchSlave-Haichusetsu-CatchRobo/src/Functions.ino
@@ -57,13 +57,40 @@ void debug_dataprint(HardwareSerial *ser){
 */
 //エアシリのボタン[指3本,手首変形]
 const int BUTTON_AIR[2]={0,2};
+//エアシリのdigital出力ピン番号
+const int PIN_AIR[2]={22,24};
 
 void air_setup(){
+  pinMode(PIN_AIR[0],OUTPUT);
+  pinMode(PIN_AIR[1],OUTPUT);
+}
+
+void air_output(){
+  digitalWrite(PIN_AIR[0],data[BUTTON_AIR[0]])
+  digitalWrite(PIN_AIR[1],data[BUTTON_AIR[1]])
+}
+
+// DCMotor ----------------------------------------------------
+/**
+ * モーター番号
+ * 横移動…0番
+ * 前後移動…1番
+ * 台座回転…2番
+*/
+//モーターのボタン[右移動,左移動,前移動,後移動,時計回り回転,反時計回り回転]
+const int BUTTON_MOTOR[6]={16,17,18,19,20,21};
+//モーターのDIR出力ピン番号
+const int PIN_MDIR[3]={30,32,34};
+const int PIN_MDIR[3]={2,3,5};
+
+void motor_setup(){
   pinMode(BUTTON_AIR[0],OUTPUT);
   pinMode(BUTTON_AIR[1],OUTPUT);
 }
 
-// DCMotor ----------------------------------------------------
-
+void motor_output(){
+  digitalWrite(PIN_AIR[0],data[BUTTON_AIR[0]])
+  digitalWrite(PIN_AIR[1],data[BUTTON_AIR[1]])
+}
 
 // PHYSICAL SWITCH --------------------------------------------
