2b00dffad08084ef2eec633014ae0b52b69f2a42
masayasuda <masayasuda137@gmail.com>
2023-09-16 07:31:13 +0900

    ok

---
+++ M5Stack-MiniHand-Haichusetsu-CatchRobo2023/.pio/build/m5stack-core2/FrameworkArduino/libb64/cencode.c.d
@@ -0,0 +1,3 @@
+.pio/build/m5stack-core2/FrameworkArduino/libb64/cencode.c.o: \
+ C:/Users/masab/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
+ C:/Users/masab/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
---
+++ M5Stack-MiniHand-Haichusetsu-CatchRobo2023/.pio/build/m5stack-core2/lib41e/PS4-esp32/ps4_parser.c.d
@@ -0,0 +1,12 @@
+.pio/build/m5stack-core2/lib41e/PS4-esp32/ps4_parser.c.o: \
+ lib/PS4-esp32/src/ps4_parser.c \
+ C:/Users/masab/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_system/include/esp_system.h \
+ C:/Users/masab/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_err.h \
+ C:/Users/masab/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_compiler.h \
+ C:/Users/masab/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_attr.h \
+ C:/Users/masab/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_bit_defs.h \
+ C:/Users/masab/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_idf_version.h \
+ C:/Users/masab/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_mac.h \
+ C:/Users/masab/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_chip_info.h \
+ C:/Users/masab/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_hw_support/include/esp_random.h \
+ lib/PS4-esp32/src/ps4.h lib/PS4-esp32/src/ps4_int.h
